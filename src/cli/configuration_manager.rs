f392555ff57a40b72e2afc9999bf329cbd5d0aeb
Florian Becker <florian00m.becker@gmail.com>
2023-05-23 11:13:26 +0000

    added configuration manager

---
+++ src/cli/configuration_manager.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+
+use confy::load;
+use serde_derive::{Deserialize, Serialize};
+
+use crate::Errors;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AppConfig {
+    pub version: String,
+    pub installed_programs: HashMap<String, Vec<String>>,
+}
+
+impl Drop for AppConfig {
+    fn drop(&mut self) {
+        let result = confy::store("dpm", "settings", self);
+        println!("saving config");
+        if result.is_err() { // can not panic
+            eprintln!("Could not save config file: {}", result.unwrap_err());
+        }
+    }
+}
+
+impl Default for AppConfig {
+    fn default() -> Self {
+        Self {
+            version: str!(env!("CARGO_PKG_VERSION")),
+            installed_programs: HashMap::new(),
+        }
+    }
+}
+
+pub fn get_config() -> Result<AppConfig, Errors> {
+    load::<AppConfig>("dpm", "settings").or_else(|e| {
+        eprintln!("Could not load config.\n{e}");
+        Err(Errors::ConfigLoadFailed)
+    })
+}
